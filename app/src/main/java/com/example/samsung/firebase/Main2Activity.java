f09367e8acc883db87eb50c5866da08f23d7f77c
deepashabirla <birladeepasha99@gmail.com>
2018-09-26 22:41:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/samsung/firebase/Main2Activity.java
@@ -0,0 +1,35 @@
+package com.example.samsung.firebase;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Main2Activity extends AppCompatActivity  {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+}
+
+
